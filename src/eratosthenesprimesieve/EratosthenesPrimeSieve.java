e44ed2f8c390cc731a30206b26f7cebeb3769ade
Flo <Flo@HP-Auer.htl.grieskirchen.local>
2019-09-20 11:44:10 +0200

    toprintPrimes

--- src/eratosthenesprimesieve/EratosthenesPrimeSieve.java
+++ src/eratosthenesprimesieve/EratosthenesPrimeSieve.java
@@ -12,7 +12,9 @@ import java.util.Scanner;
  * @author Flo
  */
 public class EratosthenesPrimeSieve {
+
     static final Scanner s = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
@@ -20,44 +22,64 @@ public class EratosthenesPrimeSieve {
         System.out.println("1.....Zahl prüfen");
         System.out.println("2.....Zahlen bis Limit ausgeben");
         int count = 0;
-        while(count == 1||count == 2)
-        {
+        while (count == 1 || count == 2) {
             count = s.nextInt();
-            
-            
+
         }
-         
-        if(count == 1)
-        {
+
+        if (count == 1) {
             System.out.println("Geben sie die zu prüfende Zahl ein:");
             count = s.nextInt();
-            if(prime(count))
-            {
+            if (prime(count)) {
                 System.out.println(count + " is a prime!");
-            }
-            else
-            {
+            } else {
                 System.out.println(count + " is not a prime!");
             }
-            
-        }
-        else if(count == 2)
-        {
+
+        } else if (count == 2) {
             System.out.println("Geben sie die das Limit ein:");
             count = s.nextInt();
-            toPrime(count);
+            printPrimes(toPrime(count));
         }
         System.out.println("Bitte geben Sie die zu Prüfende Zahl ein");
-        
+
     }
-    
-    public static boolean prime(int number)
+
+    public static boolean prime(int number) {
+        if(number == 2||number == 3)
+        {
+            return true;
+        }
+        if (number % 2 == 0&&number != 2) {
+            return false;
+        }
+        for (int i = 3; i * i <= number; i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String toPrime(int number) {
+        String primeNumbers = "";
+        for (int i = 1; i <= number; i++) {
+            int counter = 0;
+            for (int num = i; num >= 1; num--) {
+                if (i % num == 0) {
+                    counter = counter + 1;
+                }
+            }
+            if (counter == 2) {
+                //Appended the Prime number to the String
+                primeNumbers = primeNumbers + i + " ";
+            }
+        }
+        return primeNumbers;
+    }
+    public static String printPrimes(String primeNumbers)
     {
-        return false;
+        System.out.println(primeNumbers);
     }
-    public static void toPrime(int number)
-    {
-        
-    }
-    
+
 }
