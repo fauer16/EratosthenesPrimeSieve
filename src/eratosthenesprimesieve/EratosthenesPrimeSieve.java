8a8ac80d92df4025c0921e8ee77a5c644d5eed0c
Flo <Flo@HP-Auer.htl.grieskirchen.local>
2019-09-26 15:19:32 +0200

    Finish

--- src/eratosthenesprimesieve/EratosthenesPrimeSieve.java
+++ src/eratosthenesprimesieve/EratosthenesPrimeSieve.java
@@ -5,6 +5,9 @@
  */
 package eratosthenesprimesieve;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 /**
@@ -24,7 +27,7 @@ public class EratosthenesPrimeSieve {
     }
 
     public static void showmenue() {
-        
+
         System.out.println("-----------------------------------------------------------------------");
         System.out.println("1.....Zahl prüfen");
         System.out.println("2.....Zahlen bis Limit ausgeben");
@@ -32,33 +35,39 @@ public class EratosthenesPrimeSieve {
         System.out.println("0.....Beenden");
         System.out.println("-----------------------------------------------------------------------");
         int count = s.nextInt();
-        while (count != 1 && count != 2&&count!=3) {
-            System.out.println("Geben sie bitte eine Zahl von 1-2 ein");
+        while (count != 1 && count != 2 && count != 3) {
+            System.out.println("Geben sie bitte eine Zahl von 1-3 ein");
             count = s.nextInt();
 
         }
 
         if (count == 1) {
-            System.out.println("Geben sie die zu prüfende Zahl ein:");
+            System.out.println("Geben Sie die zu prüfende Zahl ein:");
             count = s.nextInt();
             if (prime(count)) {
                 System.out.println(count + " is a prime!");
             } else {
                 System.out.println(count + " is not a prime!");
             }
+            System.out.println("-----------------------------------------------------------------------");
 
         } else if (count == 2) {
-            System.out.println("Geben sie die das Limit ein:");
+            System.out.println("Geben Sie das Limit ein:");
             count = s.nextInt();
             printPrimes(toPrime(count));
+            System.out.println("-----------------------------------------------------------------------");
         }
-        System.out.println("Bitte geben Sie die zu Prüfende Zahl ein");
-        System.out.println("-----------------------------------------------------------------------");
-        if(count !=0)
+        else if(count == 3)
         {
+            System.out.println("Geben Sie das Limit ein:");
+            count = s.nextInt();
+            primeaddition(count);
+            System.out.println("-----------------------------------------------------------------------");
+        }
+        if (count != 0) {
             showmenue();
         }
-        
+
     }
 
     public static boolean prime(int number) {
@@ -87,15 +96,58 @@ public class EratosthenesPrimeSieve {
             }
             if (counter == 2) {
                 //Appended the Prime number to the String
-                primeNumbers = primeNumbers + i + ", ";
+                primeNumbers = primeNumbers +", "+ i ;
             }
         }
-     
+
         return primeNumbers;
+    }
+
+    public static int numberofPrimes(int number) {
+        int counter1 = 0;
+        for (int i = 1; i <= number; i++) {
+            int counter = 0;
+            for (int num = i; num >= 1; num--) {
+                if (i % num == 0) {
+                    counter = counter + 1;
+                }
+            }
+            if (counter == 2) {
+                //Appended the Prime number to the String
+                counter1++;
+            }
+        }
+
+        return counter1;
     }
 
     public static void printPrimes(String primeNumbers) {
         System.out.println(primeNumbers);
     }
 
-}
+    public static void primeaddition(int limit) {
+
+        int[] intarray = new int[numberofPrimes(limit)];
+        Map<String,String> map = new HashMap<>();
+        String[] split = toPrime(limit).split(", ");
+        for (int i = 0; i < split.length; i++) {
+            intarray[i] = Integer.parseInt(split[i]);
+        }
+        for (int i = 0; i < intarray.length; i++) {
+            for (int j = 0; j < intarray.length; j++) {
+                int sum = intarray[i]+intarray[j];
+                if(sum%2 == 0)
+                {
+                    
+                    System.out.println(intarray[i] +" + " + intarray[j] + "= " +sum);
+                    }
+                }
+                
+                
+            }
+            
+        }
+
+    }
+
+
